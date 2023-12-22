edaad3f0c9a983bc936ff613a4d256e39b433085
blue-slushy9 <ra.cruz971@gmail.com>
2023-12-22 15:18:06 -0800

    gotta do something else rn

--- overwrite_if_diff2.ps1
+++ overwrite_if_diff2.ps1
@@ -1,6 +1,5 @@
-# This is a PS script that will copy items from a source directory to a
-# destination directory, only if the item is not already present in the 
-# destination, or if the item is present but is an older version of the file;
+# This is a copy of my actual script, I am just using this for debugging,
+# I am going to make a few changes to the script to see if it will run;
 
 # Create your first dictionary, for the source drive, that will store your 
 # checksum values for the first drive;
@@ -13,19 +12,21 @@ $dictionary1 = [Ordered]@{}
 function Get-Checksums($path, $dictionary) {
   Get-ChildItem $path -Recurse | ForEach-Object {
     if ($_.PSIsContainer) {  # If it's a directory
-      if ($($_.FullName[0]) -ne ".") { # If the name of the directory does not begin with a "."
-        Get-Checksums $_.FullName  # Recursively call the function on the subdirectory
+      #Write-Output $_.FullName
+      if ($($_.FullName[3]) -ne ".") { # If the name of the directory does not begin with a "."
+        Write-Output $_.FullName
+        #Get-Checksums $_.FullName  # Recursively call the function on the subdirectory
       } else { # Else, if it's not a directory....
-      
+        if ($($_.FullName[]))
       # The key will be the full filepath of the file;
       #$dictionary1.Add(key = $($_.FullName))
 
         # Calculate the MD5 hash of the file
-        $hash = (Get-FileHash -Path $($_.FullName) -Algorithm MD5).Hash
+        #$hash = (Get-FileHash -Path $($_.FullName) -Algorithm MD5).Hash
 
         # Print to terminal for debugging purposes
         Write-Output "Processing file: $($_.FullName)"
-        Write-Output "Calculated hash: $hash"
+        #Write-Output "Calculated hash: $hash"
 
         # Print to terminal for debugging purposes
         #Write-Output $_.FullName, $hash
@@ -34,7 +35,7 @@ function Get-Checksums($path, $dictionary) {
         #$dictionary.Add($_.FullName, $hash)
         
         # This might be simpler, but it will replace the key if it already exists;
-        $dictionary[$_.FullName] = $hash
+        $dictionary[$_.FullName] = "x"
 
       }
     } 
@@ -54,24 +55,3 @@ Get-Checksums "E:\" ([ref]$dictionary1)
 $dictionary1.GetEnumerator() | ForEach-Object {
   Write-Output ("$($_.Key): $($_.Value)")
 }
-
-# Using Format-Table;
-#$dictionary1 | Format-Table
-
-# This command does not print the actual contents of the dictionary;
-#Write-Host "$dictionary1"
-
-<#
-# Call the function that calculates the checksums, enter the letter of your second drive;
-calc-chcksums "E:\"
-
-for file in dictionary1 {
-  if file not in dictionary2:
-    Copy-Item file -Force
-  else if file in dictionary2:
-    if file[value1] != file[value2]
-      Copy-Item file -Force
-    else do-nothing
-
-}
-#>
